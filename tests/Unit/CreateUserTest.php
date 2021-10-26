6b1e7511e0477773126a24680e03db52c6b7716e
SavelievMiron <savelevmiron@gmail.com>
2021-10-26 17:53:06 +0300

    created Unit test 'CreateUser'

---
+++ tests/Unit/CreateUserTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use App\Services\UserService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateUserTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+
+    protected UserService $userService;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->userService = $this->app->make(UserService::class);
+    }
+
+    public function testCreateUser()
+    {
+        $data = [
+          'email' => $this->faker->safeEmail(),
+          'password' => $this->faker->password()
+        ];
+
+        $user = $this->userService->createUser($data);
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertDatabaseHas('users', [
+            'email' => $data['email'],
+            'password' => $data['password']
+        ]);
+    }
+}
